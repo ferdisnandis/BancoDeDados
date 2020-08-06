ed586d0c5306306c448cf3e2312d54e792462ec5
Fernanda <61585217+fernandacarvalhodev@users.noreply.github.com>
2020-08-06 19:04:38 -0300

    Aula 09 feita

--- 02_DQL.sql
+++ 02_DQL.sql
@@ -15,3 +15,12 @@ SELECT * FROM artista ORDER BY nome DESC;
 
 -- Entre valores específicos
 SELECT * FROM album WHERE dataLançamento BETWEEN '2016-01-01' AND '2016-12-31';
+
+SELECT 
+	album.nome,
+	estilo.estilo,
+	estiloAlbum.IdEstiloAlbum
+
+FROM estiloAlbum
+	INNER JOIN album ON estiloAlbum.IdAlbum = album.IdAlbum
+	INNER JOIN estilo ON estiloAlbum.IdEstilo = estilo.IdEstilo;
